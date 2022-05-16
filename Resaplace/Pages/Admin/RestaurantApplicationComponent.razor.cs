23327db65bc318b97e1318d5b394b31656759b66
Georgi Byutyunev <78198180+gbutunev@users.noreply.github.com>
2022-05-16 22:05:21 +0300

    fix component naming

--- Resaplace/Pages/Admin/RestaurantApplicationComponent.razor.cs
+++ Resaplace/Pages/Admin/RestaurantApplicationComponent.razor.cs
@@ -6,13 +6,13 @@ namespace Resaplace.Pages.Admin
     public partial class RestaurantApplicationComponent : ComponentBase
     {
         [Parameter]
-        public RestaurantApplication Application { get; set; }
+        public RestaurantApplication RestaurantApplication { get; set; }
 
         private string mainImagePath = string.Empty;
 
         protected override void OnParametersSet()
         {
-            string imgName = Application.Images.First().ImagePath;
+            string imgName = RestaurantApplication.Images.First().ImagePath;
             mainImagePath = Path.Combine("/", "images", imgName);
         }
     }
